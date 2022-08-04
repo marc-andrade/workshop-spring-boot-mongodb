514d4af305f92d34fc622af69d24916a8e6d8971
Marcos Andrade <marcos_q_andrade@hotmail.com>
2022-08-04 12:16:23 -0300

    Consulta simples com query methods

--- src/main/java/com/nelioalves/workshopmongo/resources/PostResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/PostResource.java
@@ -1,13 +1,13 @@
 package com.nelioalves.workshopmongo.resources;
 
 import com.nelioalves.workshopmongo.domain.Post;
+import com.nelioalves.workshopmongo.resources.util.URL;
 import com.nelioalves.workshopmongo.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
@@ -18,5 +18,11 @@ public class PostResource {
     @GetMapping("/{id}")
     public ResponseEntity<Post> findById(@PathVariable String id){
         return ResponseEntity.ok().body(service.findById(id));
+    }
+
+    @GetMapping("/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        return ResponseEntity.ok().body(service.findByTitle(text));
     }
 }
--- src/main/java/com/nelioalves/workshopmongo/service/PostService.java
+++ src/main/java/com/nelioalves/workshopmongo/service/PostService.java
@@ -6,6 +6,8 @@ import com.nelioalves.workshopmongo.resources.exceptions.ObjectNotFoundException
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PostService {
 
@@ -15,6 +17,10 @@ public class PostService {
     public Post findById(String id){
         return repository.findById(id).orElseThrow(() ->
                 new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<Post> findByTitle(String text){
+        return repository.findByTitleContainingIgnoreCase(text);
     }
 
 }
